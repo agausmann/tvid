0007efb2431546585d3f06843fcc987a3ce68576
Adam Gausmann <adam@gaussian.dev>
2023-12-26 17:49:09 -0600

    Prototype graph-based image segmentation

---
+++ src/crhash.rs
@@ -0,0 +1,4 @@
+//! Cropping-resistant image hashing.
+//!
+//! Implemented based on the paper "Efficient Cropping-Resistant Robust Image
+//! Hashing" by Steinebach, Liu and Yannikos (2014)
---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+pub mod crhash;
+pub mod gbis;
