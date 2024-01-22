040312dcdbcca68ef4bb6ef4603cf1674a4cbee5
Adam Gausmann <adam@gaussian.dev>
2024-01-21 18:24:16 -0600

    Implement cropping and fast resize

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use anyhow::Context;
 use clap::Parser;
+use fast_image_resize::{DynamicImageView, DynamicImageViewMut, ImageView, ImageViewMut, Resizer};
 use ffmpeg_next as ffmpeg;
-use image::{
-    imageops::{flip_horizontal, flip_vertical, rotate90},
-    ColorType, DynamicImage, GrayImage, RgbImage, RgbaImage,
-};
-use image_hasher::{HasherConfig, Image, ImageHash};
+use image::{EncodableLayout, GrayImage};
+use image_hasher::{HasherConfig, ImageHash};
 use serde::{Deserialize, Serialize};
 use std::{
+    cmp::Ordering,
     fs::File,
     io::{stdout, BufReader, BufWriter, Write},
+    num::NonZeroU32,
     path::{Path, PathBuf},
+    str::FromStr,
 };
 
 #[derive(Debug, clap::Parser)]
@@ -18,24 +20,44 @@ struct Args {
     command: Command,
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Aspect {
+    width: u32,
+    height: u32,
+}
+
+impl FromStr for Aspect {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (width, height) = s.split_once(":").context("missing colon in aspect ratio")?;
+        Ok(Aspect {
+            width: width.parse().context("width is not a valid integer")?,
+            height: height.parse().context("height is not a valid integer")?,
+        })
+    }
+}
+
 #[derive(Debug, Clone, clap::Subcommand)]
 enum Command {
-    Hash {
-        video: PathBuf,
-        #[clap(short, long)]
-        output: Option<PathBuf>,
-    },
-    Compare {
-        tvid: PathBuf,
-        image: PathBuf,
-    },
+    Hash(HashArgs),
+    Compare { tvid: PathBuf, image: PathBuf },
+}
+
+#[derive(Debug, Clone, clap::Parser)]
+struct HashArgs {
+    video: PathBuf,
+    #[clap(short, long)]
+    crop_aspect: Option<Aspect>,
+    #[clap(short, long)]
+    output: Option<PathBuf>,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     match &args.command {
-        Command::Hash { video, output } => hash(video, output.as_deref()),
+        Command::Hash(hash_args) => hash(hash_args),
         Command::Compare { tvid, image } => compare(tvid, image),
     }
 }
@@ -74,7 +96,7 @@ mod serde_image_hash {
     }
 }
 
-fn hash(video_path: &Path, output_path: Option<&Path>) -> anyhow::Result<()> {
+fn hash(args: &HashArgs) -> anyhow::Result<()> {
     let hash_width = 8;
     let hash_height = 8;
     let hasher_config = HasherConfig::new()
@@ -83,7 +105,7 @@ fn hash(video_path: &Path, output_path: Option<&Path>) -> anyhow::Result<()> {
     let hasher = hasher_config.to_hasher();
 
     ffmpeg::init().unwrap();
-    let mut ictx = ffmpeg::format::input(&video_path)?;
+    let mut ictx = ffmpeg::format::input(&args.video)?;
     let input = ictx
         .streams()
         .best(ffmpeg::media::Type::Video)
@@ -101,13 +123,45 @@ fn hash(video_path: &Path, output_path: Option<&Path>) -> anyhow::Result<()> {
         decoder.format(),
         decoder.width(),
         decoder.height(),
-        ffmpeg::format::Pixel::RGB24,
-        400,
-        300,
-        // decoder.width(),
-        // decoder.height(),
+        ffmpeg::format::Pixel::GRAY8,
+        decoder.width(),
+        decoder.height(),
         ffmpeg::software::scaling::flag::Flags::BILINEAR,
     )?;
+
+    let (crop_x, crop_y, crop_width, crop_height) = match args.crop_aspect.map(|aspect| {
+        (
+            aspect,
+            (decoder.width() * aspect.height).cmp(&(decoder.height() * aspect.width)),
+        )
+    }) {
+        Some((aspect, Ordering::Less)) => {
+            // Crop top and bottom
+            let target_height = decoder.width() * aspect.height / aspect.width;
+            (
+                0,
+                (decoder.height() - target_height) / 2,
+                decoder.width(),
+                target_height,
+            )
+        }
+        Some((aspect, Ordering::Greater)) => {
+            // Crop left and right
+            let target_width = decoder.height() * aspect.width / aspect.height;
+            (
+                (decoder.width() - target_width) / 2,
+                0,
+                target_width,
+                decoder.height(),
+            )
+        }
+        Some((_, Ordering::Equal)) | None => (0, 0, decoder.width(), decoder.height()),
+    };
+
+    let mut resizer = Resizer::new(fast_image_resize::ResizeAlg::Convolution(
+        fast_image_resize::FilterType::Bilinear,
+    ));
+    let mut resized_image = GrayImage::new(hash_width, hash_height);
 
     let mut frame_index = 0;
 
@@ -116,36 +170,58 @@ fn hash(video_path: &Path, output_path: Option<&Path>) -> anyhow::Result<()> {
     let mut receive_and_process_decoded_frames =
         |decoder: &mut ffmpeg::decoder::Video| -> Result<(), ffmpeg::Error> {
             let mut decoded = ffmpeg::util::frame::video::Video::empty();
-            let mut rgb_frame = ffmpeg::util::frame::video::Video::empty();
+            let mut gray_frame = ffmpeg::util::frame::video::Video::empty();
             while decoder.receive_frame(&mut decoded).is_ok() {
-                scaler.run(&decoded, &mut rgb_frame)?;
-                dbg!(rgb_frame.width());
-                dbg!(rgb_frame.height());
-                dbg!(rgb_frame.stride(0));
-                dbg!(rgb_frame.data(0).len());
+                scaler.run(&decoded, &mut gray_frame)?;
 
-                let mut packed =
-                    vec![0u8; 3 * rgb_frame.width() as usize * rgb_frame.height() as usize];
-                let src_stride = rgb_frame.stride(0) as usize;
-                let dest_stride = 3 * rgb_frame.width() as usize;
-                for row in 0..rgb_frame.height() as usize {
-                    packed[row * dest_stride..][..dest_stride]
-                        .copy_from_slice(&rgb_frame.data(0)[row * src_stride..][..dest_stride]);
+                let mut pack_and_crop = vec![0u8; (crop_width * crop_height) as usize];
+                let src_stride = gray_frame.stride(0) as usize;
+                let dest_stride = crop_width as usize;
+                for row in 0..crop_height as usize {
+                    pack_and_crop[row * dest_stride..][..dest_stride].copy_from_slice(
+                        &gray_frame.data(0)
+                            [(row + crop_y as usize) * src_stride + (crop_x as usize)..]
+                            [..dest_stride],
+                    );
                 }
-                let mut file = File::create(format!("data/debug/{}.ppm", frame_index)).unwrap();
-                file.write_all(
-                    format!("P6\n{} {}\n255\n", rgb_frame.width(), rgb_frame.height()).as_bytes(),
-                )
-                .unwrap();
-                file.write_all(&packed).unwrap();
+                // let mut file = File::create(format!("data/debug/{}.ppm", frame_index)).unwrap();
+                // file.write_all(
+                //     format!("P6\n{} {}\n255\n", rgb_frame.width(), rgb_frame.height()).as_bytes(),
+                // )
+                // .unwrap();
+                // file.write_all(&packed).unwrap();
 
-                let rgb_image =
-                    RgbImage::from_raw(rgb_frame.width(), rgb_frame.height(), packed).unwrap();
-                rgb_image
-                    .save(format!("data/debug/{frame_index}.jpg"))
+                let gray_image =
+                    GrayImage::from_raw(crop_width, crop_height, pack_and_crop).unwrap();
+                // gray_image
+                //     .save(format!("data/debug/{frame_index}.jpg"))
+                //     .unwrap();
+
+                resizer
+                    .resize(
+                        &DynamicImageView::U8(
+                            ImageView::from_buffer(
+                                NonZeroU32::new(gray_image.width()).unwrap(),
+                                NonZeroU32::new(gray_image.height()).unwrap(),
+                                gray_image.as_bytes(),
+                            )
+                            .unwrap(),
+                        ),
+                        &mut DynamicImageViewMut::U8(
+                            ImageViewMut::from_buffer(
+                                NonZeroU32::new(resized_image.width()).unwrap(),
+                                NonZeroU32::new(resized_image.height()).unwrap(),
+                                resized_image.as_mut(),
+                            )
+                            .unwrap(),
+                        ),
+                    )
                     .unwrap();
+                // resized_image
+                //     .save(format!("data/debug/{frame_index}-resized.png"))
+                //     .unwrap();
 
-                let frame_hash = hasher.hash_image(&rgb_image);
+                let frame_hash = hasher.hash_image(&gray_image);
                 hashes.push(SerdeImageHash(frame_hash));
                 dbg!(frame_index);
                 frame_index += 1;
@@ -167,7 +243,7 @@ fn hash(video_path: &Path, output_path: Option<&Path>) -> anyhow::Result<()> {
         hashes,
     };
 
-    match output_path {
+    match &args.output {
         Some(path) => serde_json::to_writer(BufWriter::new(File::create(path)?), &tvid)?,
         None => serde_json::to_writer(stdout(), &tvid)?,
     }
@@ -178,8 +254,6 @@ fn hash(video_path: &Path, output_path: Option<&Path>) -> anyhow::Result<()> {
 fn compare(tvid_path: &Path, image_path: &Path) -> anyhow::Result<()> {
     let tvid: Tvid = serde_json::from_reader(BufReader::new(File::open(tvid_path)?))?;
     let image = image::open(image_path)?;
-
-    // let image = flip_vertical(&rotate90(&image));
 
     let hasher = tvid.hasher_config.to_hasher();
     let image_hash = hasher.hash_image(&image);
